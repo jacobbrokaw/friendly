14f7153e3cd1886f2d1f53f8d47f82df7c525b23
jacobbrokaw <brokawjn@gmail.com>
2019-12-02 11:12:49 -0600

    Add project files.

---
+++ Friendly.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Friendly.UI.MVC.Startup))]
+namespace Friendly.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
