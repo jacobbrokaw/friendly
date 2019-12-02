4a5a38c0413186ebcca156971e9330fe799fb247
jacobbrokaw <brokawjn@gmail.com>
2019-12-02 14:04:17 -0600

    Configured Identity for custom roles

--- Friendly.UI.MVC/Models/AccountViewModels.cs
+++ Friendly.UI.MVC/Models/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Friendly.UI.MVC.Models
+namespace IdentitySample.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
--- Friendly.UI.MVC/Startup.cs
+++ Friendly.UI.MVC/Startup.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Friendly.UI.MVC.Startup))]
-namespace Friendly.UI.MVC
+namespace IdentitySample
 {
     public partial class Startup
     {
