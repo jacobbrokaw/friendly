1ddf42ad9ac40ab42ee30e500f1c9e9fb2bf4f77
jacobbrokaw <brokawjn@gmail.com>
2019-12-04 10:32:29 -0600

    Added appropriate metadata

--- Friendly.DATA.EF/Metadata/FriendlyMetadata.cs
+++ Friendly.DATA.EF/Metadata/FriendlyMetadata.cs
@@ -10,11 +10,29 @@ namespace Friendly.DATA.EF//.Metadata
 	#region Application
 	public class ApplicationMetadata
 	{
+		[Required]
+		[Display(Name = "Open Friendship")]
 		public int OpenFriendshipId { get; set; }
+
+		[Required]
+		[Display(Name = "Applicant")]
 		public string UserId { get; set; }
+
+		[Required]
+		[Display(Name = "Date")]
+		[DisplayFormat(DataFormatString = "{0:d}")]
 		public System.DateTime ApplicationDate { get; set; }
+
+		[Display(Name = "Superior Notes")]
+		[StringLength(2000, ErrorMessage = "Notes must be a maximum 2000 characters")]
 		public string ManagerNotes { get; set; }
+
+		[Required]
+		[Display(Name = "Status")]
 		public int ApplicationStatusId { get; set; }
+
+		[Required]
+		[Display(Name = "Resume")]
 		public string ResumeFilename { get; set; }
 	}
 	[MetadataType(typeof(ApplicationMetadata))]
@@ -24,7 +42,12 @@ namespace Friendly.DATA.EF//.Metadata
 	#region ApplicationStatus
 	public class ApplicationStatusMetadata
 	{
+		[Required]
+		[Display(Name = "Status")]
 		public string StatusName { get; set; }
+
+		[Display(Name = "Description")]
+		[StringLength(250, ErrorMessage = "The description must be a maximum of 250 characters")]
 		public string StatusDescription { get; set; }
 	}
 	[MetadataType(typeof(ApplicationStatusMetadata))]
@@ -34,10 +57,24 @@ namespace Friendly.DATA.EF//.Metadata
 	#region Clique
 	public class CliqueMetadata
 	{
+
+		[Required]
+		[Display(Name = "Clique")]
+		[StringLength(25, ErrorMessage = "The name must be a maximum of 25 characters")]
 		public string Name { get; set; }
+
+		[Required]
+		[StringLength(50, ErrorMessage = "The city must be a maximum of 50 characters")]
 		public string City { get; set; }
+
+		[Required]
+		[StringLength(2, ErrorMessage = "Please use the two letter state abbreviation", MinimumLength = 2)]
 		public string State { get; set; }
+
+		[Required]
+		[Display(Name = "Superior")]
 		public string BetaId { get; set; }
+
 	}
 	[MetadataType(typeof(CliqueMetadata))]
 	public partial class Clique { }
@@ -46,7 +83,14 @@ namespace Friendly.DATA.EF//.Metadata
 	#region Friendship
 	public class FriendshipMetadata
 	{
+
+		[Required]
+		[Display(Name = "Position")]
+		[StringLength(50, ErrorMessage = "The position must be a maximum of 50 characters")]
 		public string Title { get; set; }
+
+		[Required]
+		[UIHint("MultilineText")]
 		public string Description { get; set; }
 	}
 	[MetadataType(typeof(FriendshipMetadata))]
@@ -56,7 +100,12 @@ namespace Friendly.DATA.EF//.Metadata
 	#region OpenFriendship
 	public class OpenFriendshipMetadata
 	{
+		[Required]
+		[Display(Name = "Position")]
 		public int FriendshipId { get; set; }
+
+		[Required]
+		[Display(Name = "Clique")]
 		public int CliqueId { get; set; }
 	}
 	[MetadataType(typeof(OpenFriendshipMetadata))]
@@ -66,9 +115,20 @@ namespace Friendly.DATA.EF//.Metadata
 	#region UserDetail
 	public class UserDetailMetadata
 	{
-		public string UserId { get; set; }
+		//public string UserId { get; set; }
+
+		[Required]
+		[Display(Name = "First Name")]
+		[StringLength(50, ErrorMessage = "First name must be a maximum of 50 characters")]
 		public string FirstName { get; set; }
+
+		[Required]
+		[Display(Name = "Last Name")]
+		[StringLength(50, ErrorMessage = "Last name must be a maximum of 50 characters")]
 		public string LastName { get; set; }
+
+		[Required]
+		[Display(Name = "Resume")]
 		public string ResumeFilename { get; set; }
 	}
 	[MetadataType(typeof(UserDetailMetadata))]
