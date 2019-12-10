9d28f2b71a3f50cfa6c32e263d356232311c70a8
jacobbrokaw <brokawjn@gmail.com>
2019-12-09 18:24:29 -0600

    Created contact form in "demo mode"

--- Friendly.UI.MVC/Controllers/HomeController.cs
+++ Friendly.UI.MVC/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Friendly.UI.MVC.Models;
+using Friendly.DATA.EF;
+using Microsoft.AspNet.Identity;
+using System.Net.Mail;
+using System.Net;
 
 namespace Friendly.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        FriendlyEntities db = new FriendlyEntities();
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -20,11 +28,58 @@ namespace Friendly.UI.MVC.Controllers
         }
 
         [HttpGet]
-        public ActionResult Contact()
+        public ActionResult Support()
         {
-            ViewBag.Message = "Your contact page.";
+            SupportViewModel supportModel = new SupportViewModel();
+            supportModel.Name = db.UserDetails.Find(User.Identity.GetUserId()).FullName;
+            supportModel.Email = User.Identity.Name;
 
-            return View();
+            return View(supportModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Support(SupportViewModel svm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(svm);
+            }
+
+            // Enforcing user info
+            svm.Name = db.UserDetails.Find(User.Identity.GetUserId()).FullName;
+            svm.Email = User.Identity.Name;
+
+            // Building Message
+            string message = $"<h1>New Message</h1><h3>From <a href=\"mailto:{svm.Email}\">{svm.Name}</a></h3><hr><h3>{svm.Subject}</h3><p>{svm.Message}</p>";
+
+            MailMessage mm = new MailMessage("sender@email.com", "receiver@email.com", svm.Subject, message)
+            {
+                IsBodyHtml = true,
+                Priority = MailPriority.High
+            };
+            mm.ReplyToList.Add(svm.Email);
+
+            SmtpClient client = new SmtpClient("mail.jacobbrokaw.com")
+            {
+                Credentials = new NetworkCredential("username", "password")
+            };
+
+            try
+            {
+                //client.Send(mm);
+                TempData["Demo"] = true;
+            }
+            catch (Exception ex)
+            {
+                // Didn't work. Send the error and give the inputs back.
+                ViewBag.ContactError = $"<h4>Uh oh. An error occurred.</h4>Here's what happened:{ex.StackTrace}";
+                return View(svm);
+            }
+
+            // Runs if no issues
+            TempData["SentMail"] = true;
+            return RedirectToAction("Support");
         }
     }
 }
---
+++ Friendly.UI.MVC/Models/SupportViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Friendly.UI.MVC.Models
+{
+    public class SupportViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
