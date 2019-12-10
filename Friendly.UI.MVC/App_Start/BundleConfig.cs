0fa818bd7df63c0c1a5327a7a33711388f14dbdd
jacobbrokaw <brokawjn@gmail.com>
2019-12-09 18:25:05 -0600

    Implemented Materialize with base styling

--- Friendly.UI.MVC/App_Start/BundleConfig.cs
+++ Friendly.UI.MVC/App_Start/BundleConfig.cs
@@ -18,13 +18,12 @@ namespace Friendly.UI.MVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                      "~/Scripts/materialize.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/materialize.min.css",
+                      "~/Content/style.css"));
         }
     }
 }
